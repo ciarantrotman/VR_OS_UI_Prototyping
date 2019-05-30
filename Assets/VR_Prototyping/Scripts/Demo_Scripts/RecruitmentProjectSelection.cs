d2e4d0aa1bcf1a7ddb43bf7780ae3aeaa61fe2f3
Ciaran Trotman <ciarantrotman@gmail.com>
2019-05-30 21:33:20 +0100

    Nighly Home Sync - Add Recruitment Demo

---
+++ Assets/VR_Prototyping/Scripts/Demo_Scripts/RecruitmentProjectSelection.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace VR_Prototyping.Scripts.Demo_Scripts
+{
+    public class RecruitmentProjectSelection : SelectableObject
+    {
+        [BoxGroup] public Transform table;
+        [BoxGroup] public Transform building;
+        [BoxGroup] public Transform target;
+        [BoxGroup] public Transform dome;
+        
+        protected override void InitialiseOverride()
+        {
+            selectEnd.AddListener(SelectBuilding);
+        }
+
+        private void SelectBuilding()
+        {
+            building.parent = null;
+            table.DOMove(new Vector3(0, -1, 0), .7f);
+            building.DOMove(target.transform.position, 2.5f);
+            building.DOScale(target.transform.localScale, 3f);
+            dome.DOScale(new Vector3(150, 150, 150), 3);
+        }
+    }
+}
---
+++ Assets/VR_Prototyping/Scripts/Demo_Scripts/RecruitmentTableRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using VR_Prototyping.Scripts.UI_Blocks;
+
+namespace VR_Prototyping.Scripts.Demo_Scripts
+{
+    public class RecruitmentTableRotation : DirectSlider
+    {
+        public Transform tableParent;
+        private void LateUpdate()
+        {
+            var y = Mathf.Lerp(0, 360, sliderValue);
+            Debug.Log(y);
+            tableParent.transform.eulerAngles =  new Vector3(0, y,0);
+        }
+    }
+}
